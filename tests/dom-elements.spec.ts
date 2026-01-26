507f503db242d677c4827ffe1bffc92401d69b2d
yishai <yishai5555@gmail.com>
2026-01-26 16:10:20 +0200

    04-parent-element-child-element-filter

--- tests/dom-elements.spec.ts
+++ tests/dom-elements.spec.ts
@@ -40,3 +40,46 @@ test("User facing locators 2", async ({ page }) => {
 
   await page.getByTitle("IoT Dashboard").click();
 });
+
+test("child element", async ({ page }) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+  await page
+    .locator("nb-card")
+    .locator("nb-radio")
+    .locator(':text-is("Option 1")')
+    .click();
+
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "Sign in" })
+    .first()
+    .click();
+});
+
+test("using index", async ({ page }) => {
+  await page.locator("nb-card").nth(3).getByRole("button").click();
+});
+
+test("parent element", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has:  page.locator(".status-danger")  })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+});
